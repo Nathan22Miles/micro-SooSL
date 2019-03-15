fa4a0345a06c97e13fc6b9d094a0729737dc5094
Nathan Miles <milesnl@gmail.com>
2019-03-15 16:23:15 -0500

    wip: use model from Tim

---
+++ src/components/positionFeatures.tsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+
+export default [
+    ['left-eye', (props: any) => <ellipse {...props} ry="16" rx="25.5" cy="138.4375" cx="242" />],
+    ['right-eye', (props: any) => <ellipse {...props} ry="16" rx="25.5" cy="139.4375" cx="329" />]
+]
